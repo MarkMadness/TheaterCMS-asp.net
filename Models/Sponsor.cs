fecdecd49c45efcff36b87c94427aca93b49d0b9
Mark <1187productions@gmail.com>
2020-11-10 16:25:00 -0700

    Uploaded Models folder of MVC project files

---
+++ Models/Sponsor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheatreCMS.Models
+{
+    public class Sponsor
+    {
+        [Key]
+        public int SponsorId { get; set; }  // sponsor primary key
+        public string Name { get; set; }    // sponsor name
+        [Display(Name ="Logo Image")]       
+        public int LogoId { get; set; }     // ID of photo.cs object
+        public int? Height { get; set; }    // logo display height (may be different from original)
+        public int? Width { get; set; }     // logo display width (may be different from original)
+        public bool Current { get; set; }   // active sponsor
+        public string Link { get; set; }        //url of sponsor website
+    }
+} 
